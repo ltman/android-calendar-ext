95deed4797173a3c9ddca01dfb672d18d8882bfa
Autthakrit.C <autthakrit@ltman.com>
2023-11-08 11:27:41 +0700

    fix app can not build

--- app/src/main/java/com/ltman/calendarextapp/MainActivity.kt
+++ app/src/main/java/com/ltman/calendarextapp/MainActivity.kt
@@ -6,9 +6,10 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.ltman.calendarext.CalendarExt
 import com.ltman.calendarext.CalendarExtConfig
-import com.ltman.calendarext.todayString
-import com.ltman.calendarext.todayWithTimeString
-import com.ltman.calendarextapp.databinding.ActivityMainBinding
+import com.ltman.calendarext.R
+import com.ltman.calendarext.databinding.ActivityMainBinding
+import com.ltman.calendarext.toTodayString
+import com.ltman.calendarext.toTodayWithTimeString
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,26 +17,27 @@ class MainActivity : AppCompatActivity() {
         context: Context,
         val now: Calendar
     ) {
-        val today = now.todayString(context)
-        val todayWithTime = now.todayWithTimeString(context)
+        val today = now.toTodayString(context)
+        val todayWithTime = now.toTodayWithTimeString(context)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        CalendarExt.configure(CalendarExtConfig(
-            months_short_array = R.array.months_short_array,
-            date_format__today = R.string.date_format__today,
-            date_format__tomorrow = R.string.date_format__tomorrow,
-            date_format__yesterday = R.string.date_format__yesterday,
-            date_format__hour = R.string.date_format__hour,
-            date_format__minute = R.string.date_format__minute,
-            date_format__now = R.string.date_format__now,
-            date_format__one_hour = R.string.date_format__one_hour,
-            date_format__one_minute = R.string.date_format__one_minute,
-            date_format__past = R.string.date_format__past,
-            date_format__past_with_hyphen = R.string.date_format__past_with_hyphen,
-            date_format__today_with_time = R.string.date_format__today_with_time,
-            date_format__tomorrow_with_time = R.string.date_format__tomorrow_with_time,
-            date_format__yesterday_with_time = R.string.date_format__yesterday_with_time
-        ))
+        CalendarExt.configure(
+            CalendarExtConfig(
+                date_format__today = R.string.date_format__today,
+                date_format__tomorrow = R.string.date_format__tomorrow,
+                date_format__yesterday = R.string.date_format__yesterday,
+                date_format__hour = R.string.date_format__hour,
+                date_format__minute = R.string.date_format__minute,
+                date_format__now = R.string.date_format__now,
+                date_format__one_hour = R.string.date_format__one_hour,
+                date_format__one_minute = R.string.date_format__one_minute,
+                date_format__past = R.string.date_format__past,
+                date_format__today_with_time = R.string.date_format__today_with_time,
+                date_format__tomorrow_with_time = R.string.date_format__tomorrow_with_time,
+                date_format__yesterday_with_time = R.string.date_format__yesterday_with_time
+            )
+        )
 
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil
